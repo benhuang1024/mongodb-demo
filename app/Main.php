6953453e868aa664f8f77d5baa14ef1b3228f455
benhuang <benhuang1024@gmail.com>
2020-07-04 12:16:44 +0800

    init mongodb

--- app/Main.php
+++ app/Main.php
@@ -7,43 +7,20 @@
 
 namespace App;
 
-use Noodlehaus\Config;
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-
 /**
  * Class main
  *
  * @package App
  */
-class Main
+class Main extends Basic
 {
-    /**
-     * @var Config
-     */
-    protected $config;
-    /**
-     * @var Logger|string
-     */
-    protected $log;
 
     /**
-     * main constructor.
-     *
-     * @throws \Exception
-     */
-    public function __construct()
-    {
-        $this->config = Config::load(__DIR__ . '/../config/app.php');
-        $this->log = new Logger('app');
-        $this->log->pushHandler(new StreamHandler(__DIR__ . '/../logs/app.log', Logger::DEBUG));
-    }
-
-    /**
-     * @return null
+     * @return |null
      */
     public function run()
     {
+        echo $_ENV['APP_NAME'] . PHP_EOL;
         $this->log->info('Hello', ['world']);
         $this->log->debug('config', (array)$this->config->all());
         return null;
